74ba8570293157d2c4a98903780e39fb5a297a63
keshav balesara <keshavbalesara04@gmail.com>
2023-10-04 01:17:26 +0530

    log out added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  NavigationType,
+  Navigate,
 } from "react-router-dom";
 
 import Home from './pages/home/Home';
@@ -26,14 +28,30 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/order" element={<Order/>} />
+        <Route path="/order" element={
+          <ProtectedRoute>
+            <Order/>
+          </ProtectedRoute>
+        } />
         <Route path="/cart" element={<Cart/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard" element={
+          <ProtectedRouteForAdmin>
+            <Dashboard/>
+          </ProtectedRouteForAdmin>
+        } />
         <Route path="/login" element={<Login/>}/>
         <Route path="signup" element={<Signup/>}/>
         <Route path="/productinfo/:id" element={<ProductInfo/>} />
-        <Route path="/addproduct" element={<AddProduct/>}/>
-        <Route path="/updateproduct" element={<UpdateProduct/>}/>
+        <Route path="/addproduct" element={
+          <ProtectedRouteForAdmin>
+            <AddProduct/>
+          </ProtectedRouteForAdmin>
+        }/>
+        <Route path="/updateproduct" element={
+          <ProtectedRouteForAdmin>
+            <UpdateProduct/>
+          </ProtectedRouteForAdmin>
+        }/>
 
         <Route path="/*" element={<NoPage/>} />
         
@@ -50,3 +68,27 @@ function App() {
 }
 
 export default App
+
+//user protected route
+export const ProtectedRoute=({children})=>{
+  const user = localStorage.getItem('user')
+  if(user){
+    return children;
+  }
+  else{
+    return (<NavigationType to='/login'/>)
+  }
+}
+
+//admin protect route
+
+const ProtectedRouteForAdmin=({children})=>{
+  const admin = JSON.parse(localStorage.getItem('user'))
+
+  if(admin.user.email === 'keshavbalesara04@gmail.com'){
+    return  children ;
+  }
+  else{
+    return <Navigate to ={'/login'}/>
+  }
+}
