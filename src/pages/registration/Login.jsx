2e14823394a9ef0e684fc11854c08b30cc7ee299
keshav balesara <keshavbalesara04@gmail.com>
2023-10-03 18:46:41 +0530

    login setup

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import myContext from '../../context/data/myContext'
 import { signInWithEmailAndPassword } from 'firebase/auth';
